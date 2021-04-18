ff098fa452e931f4b7f71d8e4b6a2e377ca58610
Moha-Sameh <ms.slama01@hotmail.com>
2021-04-18 23:53:49 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0fc0c36db17cc68e828b369ee77e4c40'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7afe4c8f-f428-49da-aaaf-35800cee7f35'
+};
